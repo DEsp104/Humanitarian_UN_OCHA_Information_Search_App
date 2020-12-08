a19f0291ab9597cdbedae0cf6c5c1a68c8420269
david espinal <despinal0425@gmail.com>
2020-12-08 15:41:41 -0500

    made changes to the response async function & added event listener to div with id title_section

--- report/redirect.js
+++ report/redirect.js
@@ -11,7 +11,14 @@ let response = async function () {
 
     const reportData = res.data.data
     for (let i = 0; i < reportData.length; i++) { 
-      document.getElementById('title_section').innerHTML += `<li id = ${reportData[i].id}><a href = './reportsinfo.html'>${reportData[i].fields.title}</a></li>`
+      let newLi = document.createElement('li');
+      let newA = document.createElement('a');
+      newA.setAttribute('href', './reportsinfo.html')
+      newA.setAttribute("id", `${reportData[i].id}`);
+      newA.textContent = `${reportData[i].fields.title}`
+    
+      newLi.appendChild(newA);
+      document.getElementById('title_section').appendChild(newLi); 
     }
   }).catch(err => { 
     console.log(err)
@@ -21,57 +28,13 @@ let response = async function () {
 response();
 
 
+//Click title, save id number to local storage
+document.getElementById('title_section').addEventListener('click', (e) => { 
+  let reportId = e.target.id;
+  console.log(reportId.id)
 
-// //When a title is clicked, the either an image or body text will appeared
-// document.getElementById('title_section').addEventListener('click', async (e) => { 
-  
-//   let reportId = e.target.id;
-
-//   let divElement = document.querySelector('div');
-//   divElement.innerHTML = "";
-
-
-// //Add the back button to get back to the list of reports title
-  
-// let newButton = document.createElement('button');
-
-//   try {
-//     const responseTwo = await axios.get(`https://api.reliefweb.int/v1/reports/${reportId}`);
-//     console.log(responseTwo)
-
-//     let responseTitle = responseTwo.data.data[0].fields.title
-//     let responseBody = responseTwo.data.data[0].fields['body-html']
-    
-
-//     let newHeaderElement = document.createElement('h3')
-//     newHeaderElement.textContent = `${responseTitle}`
-//     divElement.appendChild(newHeaderElement);
-
-//     if (responseBody === undefined) {
-//       let reponseImageUrl = responseTwo.data.data[0].fields.file[0].preview.url
-//       let newImageElement = document.createElement('img');
-//       newImageElement.setAttribute('src',`${reponseImageUrl}`);
-//       divElement.appendChild(newImageElement);
-//     } else {
-//       let newParElement = document.createElement('p');
-//       newParElement.innerHTML = `${responseBody}`
-//       divElement.appendChild(newParElement);
-//     }
-
-//     newButton.textContent = "Back"
-// newButton.setAttribute('onclick', 'reloadPage()');  
-// document.querySelector('div').appendChild(newButton);
-
-//   document.querySelector('button').addEventListener('click', function () {
-//     location.reload();
-//   })
-   
-//   } catch (e) { 
-//     console.log(e);
-//   }
-// })
-
-
+  localStorage.setItem('report-id', reportId);
+})
 
 
 //Click next or previous and you will get fresh 10 new reports
